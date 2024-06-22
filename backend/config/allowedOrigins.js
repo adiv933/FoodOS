9c4a99ed3ae3b96ed5828daf386ef221efc59ae1
Aditya Verma <adityav090304@gmail.com>
2024-06-22 13:56:10 +0530

    Update allowedOrigins#2

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -5,7 +5,10 @@ const allowedOrigins = [
     process.env.BASE_SERVER_URL,
     'https://foodos.onrender.com',
     'https://food-os.vercel.app/',
-    'https://food-h51g4opbf-clumsydog09s-projects.vercel.app'
+    'https://food-h51g4opbf-clumsydog09s-projects.vercel.app',
+    'https://food-os-clumsydog09s-projects.vercel.app',
+    'https://food-os-git-main-clumsydog09s-projects.vercel.app',
+    
 
 ]
 
