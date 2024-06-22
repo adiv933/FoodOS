db5c80bb16b25a8500d96ded7cdb864dc0b2dfe7
Aditya Verma <adityav090304@gmail.com>
2024-06-22 11:56:52 +0530

    server ready to deploy#2

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const allowedOrigins = [
     process.env.BASE_CLIENT_URL,
-    process.env.BASE_SERVER_URL
+    process.env.BASE_SERVER_URL,
+    'https://foodos.onrender.com'
 ]
 
 module.exports = allowedOrigins
