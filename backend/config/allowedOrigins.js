deb178889e1a61f3d5d65ee72aadc9c8c4865594
Aditya Verma <adityav090304@gmail.com>
2024-06-22 13:46:27 +0530

    Update allowedOrigins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 const allowedOrigins = [
     process.env.BASE_CLIENT_URL,
     process.env.BASE_SERVER_URL,
-    'https://foodos.onrender.com'
+    'https://foodos.onrender.com',
+    'https://food-os.vercel.app/',
+    'https://food-h51g4opbf-clumsydog09s-projects.vercel.app'
+
 ]
 
 module.exports = allowedOrigins
